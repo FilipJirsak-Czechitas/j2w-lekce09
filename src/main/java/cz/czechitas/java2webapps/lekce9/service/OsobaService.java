e6e872c81b368f5818a38bf7e5208b22298023b5
Filip Jirsák <filip@jirsak.org>
2021-05-26 21:48:14 +0200

    Příklad možného řešení.

--- src/main/java/cz/czechitas/java2webapps/lekce9/service/OsobaService.java
+++ src/main/java/cz/czechitas/java2webapps/lekce9/service/OsobaService.java
@@ -6,7 +6,13 @@ import cz.czechitas.java2webapps.lekce9.repository.OsobaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
 
 import java.time.LocalDate;
 
